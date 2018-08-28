f15d15587b4192de7b23b6ea1049421effac789c
magengxin <1161807772@qq.com>
2018-08-28 16:40:46 +0800

    ''

---
+++ src/components/nav.vue
@@ -0,0 +1,89 @@
+<template>
+  <div class="db_nav" :class="{fixed:isFixedHeader}">
+    <ul>
+      <li v-for="item in navItem">
+        <router-link :to="item.url">{{item.text}}</router-link>
+      </li>
+    </ul>
+  </div>
+</template>
+<script type="text/javascript">
+  import {mapGetters, mapState, mapMutations, mapActions} from 'vuex'
+
+  export default {
+    data() {
+      return {
+        navItem: [
+          {text: "首页", url: "/home"},
+          {text: "分类", url: "/category"},
+          {text: "购物车", url: "/cart"},
+          {text: "我的", url: "/my"},
+        ]
+      }
+    },
+    computed: {
+      //映射State
+      ...mapState([
+        'isFixedHeader'
+      ])
+    },
+    mounted() {
+      // 监听滚动条
+      window.addEventListener('scroll', this.handlerScroll);
+    },
+    methods: {
+      // 映射Actions中的handlerScroll方法
+      ...mapActions([
+        'handlerScroll'
+      ])
+    }
+  }
+</script>
+<style lang="scss">
+  .db_nav {
+    width: 100%;
+    height: 43px;
+    line-height: 43px;
+    border-bottom: 2px solid #3ba94d;
+    background-color: #fff;
+  }
+
+  .db_nav ul {
+    white-space: nowrap;
+    overflow-x: auto;
+    -webkit-overflow-scrolling: touch;
+    display: -o-box;
+    display: -webkit-box;
+    display: -webkit-flex;
+    display: flex;
+    width: 100%;
+  }
+
+  .db_nav ul li {
+    display: inline-block;
+    -webkit-box-flex: 1;
+    -webkit-flex: 1;
+    flex: 1;
+    vertical-align: middle;
+    text-align: center;
+  }
+
+  .db_nav ul li a.active {
+    color: #3ba94d;
+  }
+
+  .db_nav ul li a {
+    color: #4a4a4a;
+    font-size: 15px;
+  }
+
+  .db_nav.fixed {
+    z-index: 999;
+    position: fixed;
+    top: 0;
+    left: 50%;
+    transform: translateX(-50%);
+    width: 100%;
+    max-width: 620px;
+  }
+</style>
